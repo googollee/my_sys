e6b52f0e0734cb25aca7adf9abad6f1556d2f4e8
Googol Lee <i@googol.im>
2021-12-30 14:55:41 +0100

    update

---
+++ playbook/go_lsp_update.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+go install golang.org/x/tools/gopls@latest
+go install github.com/nametake/golangci-lint-langserver@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
