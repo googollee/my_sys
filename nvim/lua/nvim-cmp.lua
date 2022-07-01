18a49bf2ae08c98954a75a6214d92e4ae0c9476f
Googol Lee <i@googol.im>
2022-07-01 15:39:21 +0200

    update

--- nvim/lua/nvim-cmp.lua
+++ nvim/lua/nvim-cmp.lua
@@ -31,22 +31,8 @@ function M.init(packer)
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if not cmp.visible() then
-              fallback()
-              return
-            end
-
-            cmp.select_next_item()
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if not cmp.visible() then
-              fallback()
-              return
-            end
-
-            cmp.select_prev_item()
-          end, { 'i', 's' }),
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
