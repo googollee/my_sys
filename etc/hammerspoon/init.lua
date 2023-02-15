0e1bc808bea25fd96702db7bfabca1cf6d6d8b1f
Googol Lee <i@googol.im>
2023-02-15 17:15:05 +0100

    update

--- etc/hammerspoon/init.lua
+++ etc/hammerspoon/init.lua
@@ -1,9 +1,3 @@
-hs.loadSpoon("RecursiveBinder")
-
-spoon.RecursiveBinder.escapeKey = {{}, 'escape'}  -- Press escape to abort
-
-local singleKey = spoon.RecursiveBinder.singleKey
-
 local setFocusedWindowPosition = function(left, right, top, bottom)
   local screen = hs.mouse.getCurrentScreen()
   local win = hs.window.focusedWindow()
@@ -20,16 +14,37 @@ local setFocusedWindowPosition = function(left, right, top, bottom)
 end
 
 local windowMoving = {
-  [singleKey('`', 'full')]       = function() setFocusedWindowPosition(0,   1,   0,   1) end,
-  [singleKey('space', 'center')] = function() setFocusedWindowPosition(1/8, 7/8, 0,   1) end,
-  [singleKey('a', 'left')]       = function() setFocusedWindowPosition(0,   0.5, 0,   1) end,
-  [singleKey('s', 'bottom')]     = function() setFocusedWindowPosition(0,   1,   0.5, 1) end,
-  [singleKey('d', 'top')]        = function() setFocusedWindowPosition(0,   1,   0,   0.5) end,
-  [singleKey('f', 'right')]      = function() setFocusedWindowPosition(0.5, 1,   0,   1) end,
-  [singleKey('q', 'top-left')]     = function() setFocusedWindowPosition(0,   0.5, 0,   0.5) end,
-  [singleKey('r', 'top-right')]    = function() setFocusedWindowPosition(0.5, 1,   0,   0.5) end,
-  [singleKey('z', 'bottom-left')]  = function() setFocusedWindowPosition(0,   0.5, 0.5, 1) end,
-  [singleKey('v', 'bottom-right')] = function() setFocusedWindowPosition(0.5, 1,   0.5, 1) end,
+  [{'`', 'full'}]       = function() setFocusedWindowPosition(0,   1,   0,   1) end,
+  [{'space', 'center'}] = function() setFocusedWindowPosition(1/8, 7/8, 0,   1) end,
+  [{'a', 'left'}]       = function() setFocusedWindowPosition(0,   0.5, 0,   1) end,
+  [{'s', 'bottom'}]     = function() setFocusedWindowPosition(0,   1,   0.5, 1) end,
+  [{'d', 'top'}]        = function() setFocusedWindowPosition(0,   1,   0,   0.5) end,
+  [{'f', 'right'}]      = function() setFocusedWindowPosition(0.5, 1,   0,   1) end,
+  [{'q', 'top-left'}]     = function() setFocusedWindowPosition(0,   0.5, 0,   0.5) end,
+  [{'r', 'top-right'}]    = function() setFocusedWindowPosition(0.5, 1,   0,   0.5) end,
+  [{'z', 'bottom-left'}]  = function() setFocusedWindowPosition(0,   0.5, 0.5, 1) end,
+  [{'v', 'bottom-right'}] = function() setFocusedWindowPosition(0.5, 1,   0.5, 1) end,
 }
 
-hs.hotkey.bind({'shift'}, 'space', spoon.RecursiveBinder.recursiveBind(windowMoving))
+-- hs.loadSpoon("RecursiveBinder")
+-- local singleKey = spoon.RecursiveBinder.singleKey
+
+-- function toSingleKey(map)
+  -- local ret = {}
+  -- for key, f in pairs(map) do
+    -- ret[singleKey(key[1], key[2])] = f
+  -- end
+  -- return ret
+-- end
+
+-- spoon.RecursiveBinder.escapeKey = {{}, 'escape'}  -- Press escape to abort
+-- hs.hotkey.bind({'shift'}, 'space', spoon.RecursiveBinder.recursiveBind(toSingleKey(windowMoving)))
+
+winModal = hs.hotkey.modal.new('shift', 'space')
+winModal:bind('', 'escape', function() k:exit() end)
+for key, f in pairs(windowMoving) do
+  winModal:bind('', key[1], key[2], function()
+    f()
+    winModal:exit()
+  end)
+end
