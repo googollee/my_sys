ae4b5e8b1f2905221c0256244f5c44dd07846db5
Googol Lee <i@googol.im>
2023-07-04 15:11:26 +0200

    Update gitsigns

--- nvim/lua/git.lua
+++ nvim/lua/git.lua
@@ -1,5 +1,5 @@
 return function(packer)
   packer 'tpope/vim-fugitive' -- git
   packer 'junegunn/gv.vim' -- git log
-  packer 'airblade/vim-gitgutter' -- git diff
+  packer 'lewis6991/gitsigns.nvim' -- git diff
 end
