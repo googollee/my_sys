55ab8caa19d6e2cf1a575fb36e5e472d44b5d0eb
Googol Lee <i@googol.im>
2023-08-09 15:02:18 +0200

    Update cider

--- nvim/lua/lang/cider.lua
+++ nvim/lua/lang/cider.lua
@@ -1,10 +1,47 @@
 return function(use)
+  if vim.fn.expand('%:p'):find('/google/src/cloud', 1) == nil then
+    return
+  end
+
+  local function goog(plugin, config)
+    use({
+      name = plugin,
+      dir = "/usr/share/vim/google/" .. plugin,
+      dependencies = { "maktaba" },
+      config = config,
+    })
+  end
+
+  use {
+    name = "maktaba",
+    dir = "/usr/share/vim/google/maktaba",
+    init = function ()
+      vim.cmd("source /usr/share/vim/google/glug/bootstrap.vim")
+    end,
+  }
+  goog("core")
+  goog("glaive")
+  goog("google-filetypes")
+  goog("ft-cpp")
+  goog("ft-go")
+  goog("ft-java")
+  goog("ft-kotlin")
+  goog("ft-proto")
+  goog("ft-python")
+
+  goog("ultisnips-google")
+  goog("autogen")
+  goog("blaze")
+  goog("codefmt")
+  goog("codefmt-google")
+  goog("googlestyle")
+  goog("relatedfiles", function()
+    util = require('util')
+    util.noremap('n', "<leader>r", ":RelatedFilesWindow<cr>")
+  end)
+
 
   require("lsp").config_lsp(function()
-    if vim.fn.expand('%:p'):find('/google/src/cloud', 1) == nil then
-      return
-    end
-
     local util = require('util')
     local augroup = util.augroup
     local autocmd = util.autocmd
@@ -20,14 +57,15 @@ return function(use)
 
     configs.ciderlsp = {
       default_config = {
-        cmd = { "/google/bin/releases/cider/ciderlsp/ciderlsp", "--tooltag=nvim-cmp", "--noforward_sync_responses" },
-        filetypes = { "c", "cc", "h", "cpp", "java", "kotlin", "objc", "proto", "textproto", "go", "python", "bzl" },
-        root_dir = nvim_lsp.util.root_pattern("BUILD"),
-        settings = {},
-      },
+        cmd = { '/google/bin/releases/cider/ciderlsp/ciderlsp', '--tooltag=nvim-lsp', '--noforward_sync_responses' };
+        filetypes = { "c", "cpp", "h", "java", "kotlin", "objc", "proto", "textproto", "go", "python", "bzl" },
+        offset_encoding = 'utf-8',
+        root_dir = nvim_lsp.util.root_pattern('google3/*BUILD');
+        settings = {};
+      }
     }
 
-    cfg = {
+     cfg = {
       on_attach = require('lsp').on_attach(),
       capabilities = require('nvim-cmp').capabilities(),
     }
