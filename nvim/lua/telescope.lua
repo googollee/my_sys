4deb16d418210f776e030a9cefb826d5f3de9fee
Googol Lee <i@googol.im>
2023-10-20 14:16:38 -0700

    fix nvim

--- nvim/lua/telescope.lua
+++ nvim/lua/telescope.lua
@@ -7,7 +7,7 @@ return function(use)
 
       local util = require('util')
       util.noremap('n', '<C-p>', ':Telescope find_files<CR>')
-      util.noremap('n', '<C-f>', ':Telescope lsp_document_symbols<CR>')↴
+      util.noremap('n', '<C-f>', ':Telescope lsp_document_symbols<CR>')
       util.noremap('n', '<leader>fg', ':Telescope live_grep<CR>')
       util.noremap('n', '<leader>fb', ':Telescope buffers<CR>')
       util.noremap('n', '<leader>fh', ':Telescope help_tags<CR>')
