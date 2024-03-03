9c5474844288c0e0da31080da9f085f0965677c1
Googol Lee <i@googol.im>
2024-03-03 12:17:37 +0100

    Update samba config

--- home-cluster/01-get-config.sh
+++ home-cluster/01-get-config.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-HOST="nas"
+HOST="$1"
 
-ssh ${HOST} "sudo sed -i '/\[global\]/a \\tinclude = registry\n' /etc/samba/smb.conf; \
+if [ "${HOST}" = "" ]
+then
+  echo "usage: $0 <host>"
+  exit -1
+fi
+
+scp 01-get-config.files/smb.conf ${HOST}:~
+
+ssh ${HOST} "sudo cp ~/smb.conf /etc/samba/smb.conf; \
+sudo systemctl enable smb.service; \
 sudo systemctl restart smb.service; \
+sudo setsebool -P samba_export_all_ro=1 samba_export_all_rw=1; \
+sudo semanage fcontext –at samba_share_t '/mnt/data/media'; \
+sudo restorecon '/mnt/data/media'; \
+sudo firewall-cmd --permanent --add-service=samba; \
+sudo firewall-cmd --reload; \
 sudo systemctl enable cockpit.socket; \
 sudo systemctl start cockpit.socket; \
 sudo firewall-cmd --permanent --add-service=cockpit; \
