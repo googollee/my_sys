1f0cb64a4feacf89a73b4e694e7199810872c454
Googol Lee <i@googol.im>
2024-03-18 13:20:45 +0100

    Add golines.

--- go_lsp_update.sh
+++ go_lsp_update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 go install golang.org/x/tools/gopls@latest
+go install github.com/segmentio/golines@latest
 go install github.com/nametake/golangci-lint-langserver@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
