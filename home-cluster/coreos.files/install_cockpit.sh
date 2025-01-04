2278de0e13f89f2b7ac1fd0b0b4551d50c7c188d
Googol Lee <i@googol.im>
2025-01-04 12:00:02 +0100

    Update

--- home-cluster/coreos.files/install_cockpit.sh
+++ home-cluster/coreos.files/install_cockpit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 rpm-ostree override remove nfs-utils-coreos
-rpm-ostree install --assumeyes nfs-utils 
-rpm-ostree install --assumeyes git avahi samba virt-manager
-rpm-ostree install --assumeyes cockpit-system cockpit-ostree cockpit-podman cockpit-storaged cockpit-networkmanager cockpit-machines cockpit-selinux cockpit-files
+rpm-ostree install --assumeyes nfs-utils \
+  git avahi samba virt-manager \
+  cockpit-system cockpit-ostree cockpit-podman cockpit-storaged cockpit-networkmanager cockpit-machines cockpit-selinux cockpit-files
 podman container runlabel --name cockpit-ws RUN quay.io/cockpit/ws
 podman container runlabel INSTALL quay.io/cockpit/ws
 systemctl enable libvirtd.service
