a34cff07f75e27c0d2da58eada117379855429ba
Googol Lee <i@googol.im>
2025-02-15 09:19:45 +0100

    Update

--- home-cluster/coreos.files/install_cockpit.sh
+++ home-cluster/coreos.files/install_cockpit.sh
@@ -2,7 +2,7 @@
 
 rpm-ostree override remove nfs-utils-coreos
 rpm-ostree install --assumeyes nfs-utils \
-  git screen avahi samba virt-manager \
+  git docker-buildx screen avahi samba virt-manager \
   cockpit-system cockpit-ostree cockpit-podman cockpit-storaged cockpit-networkmanager cockpit-machines cockpit-selinux cockpit-files
 podman container runlabel --name cockpit-ws RUN quay.io/cockpit/ws
 podman container runlabel INSTALL quay.io/cockpit/ws
