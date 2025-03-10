352a15ade2d5c8de1929b933a8e34ee6c3bb10c6
Googol Lee <i@googol.im>
2025-03-10 11:25:11 +0100

    Update

--- nvim/lua/ai.lua
+++ nvim/lua/ai.lua
@@ -1,22 +1,3 @@
 return function(use)
-  util = require('util')
-
-  use {
-    'frankroeder/parrot.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'rcarriga/nvim-notify' },
-    config = function()
-      require("parrot").setup {
-        -- Providers must be explicitly added to make them available.
-        providers = {
-          ollama = {
-            topic = {
-              model = "codellama",
-              params = { max_tokens = 1024 },
-            },
-          },
-        },
-      }
-    end,
-  }
-
+  use 'github/copilot.vim'
 end
