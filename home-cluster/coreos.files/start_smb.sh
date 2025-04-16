b950c167344b5c8c17417f1dcbf69a358e087158
Googol Lee <i@googol.im>
2025-04-16 20:03:53 +0200

    Update

--- home-cluster/coreos.files/start_smb.sh
+++ home-cluster/coreos.files/start_smb.sh
@@ -3,6 +3,6 @@
 sudo systemctl enable smb.service nmb.service
 sudo systemctl restart smb.service nmb.service
 sudo setsebool -P samba_export_all_ro=1 samba_export_all_rw=1
-sudo semanage fcontext –at samba_share_t '/mnt/data/media'
+sudo semanage fcontext -a -t samba_share_t '/mnt/data/media(/.*)?'
 sudo restorecon '/mnt/data/media'
 sudo smbpasswd -a googol
