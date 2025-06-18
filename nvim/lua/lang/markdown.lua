f289f8b7efedbc60b02de602952d7b2f9c61373e
Googol Lee <i@googol.im>
2025-06-18 07:10:25 +0200

    Update

--- nvim/lua/lang/markdown.lua
+++ nvim/lua/lang/markdown.lua
@@ -4,4 +4,13 @@ return function(add, now, later)
   vim.g.vim_markdown_json_frontmatter = 1
   vim.g.vim_markdown_new_list_item_indent = 2
   vim.g.vim_markdown_folding_level = 6
+
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('my.lsp', {}),
+    callback = function(args)
+      require('lsp').on_attach(args)
+    end
+  })
+
+  vim.lsp.enable('markdown')
 end
