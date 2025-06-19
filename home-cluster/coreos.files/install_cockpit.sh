624c1b91cae67b128db782030414e208027ab626
Googol Lee <i@googol.im>
2025-06-19 12:03:11 +0200

    Update

--- home-cluster/coreos.files/install_cockpit.sh
+++ home-cluster/coreos.files/install_cockpit.sh
@@ -7,6 +7,7 @@ rpm-ostree install --assumeyes nfs-utils \
 podman container runlabel --name cockpit-ws RUN quay.io/cockpit/ws
 podman container runlabel INSTALL quay.io/cockpit/ws
 
-# Enable units after rebooting
-# systemctl enable libvirtd.service
-# systemctl enable cockpit.service
+echo Enable units after rebooting
+echo $ sudo passwd googol
+echo $ systemctl enable libvirtd.service
+echo $ systemctl enable cockpit.service
