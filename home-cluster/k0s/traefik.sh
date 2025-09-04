dd8b201b692e08d4e021a57b80f010fe640eb056
Googol Lee <i@googol.im>
2025-09-04 09:28:46 +0200

    Update services.

--- home-cluster/k0s/traefik.sh
+++ home-cluster/k0s/traefik.sh
@@ -1,2 +1,2 @@
-helm template --namespace=infra-traefik traefik traefik/traefik-crds > traefik-crds.yaml
-helm template --debug --namespace=infra-traefik --values=traefik.values.yaml traefik traefik/traefik > traefik.yaml
+helm template --namespace=infra traefik traefik/traefik-crds > traefik-crds.yaml
+helm template --debug --namespace=infra --values=traefik.values.yaml traefik traefik/traefik > traefik.yaml
