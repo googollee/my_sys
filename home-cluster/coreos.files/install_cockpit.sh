b690a026375698212aa66d87ba270a187c59dd94
Googol Lee <i@googol.im>
2025-09-05 11:17:35 +0200

    Update coreos init.

--- home-cluster/coreos.files/install_cockpit.sh
+++ home-cluster/coreos.files/install_cockpit.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-rpm-ostree override remove nfs-utils-coreos
-rpm-ostree install --assumeyes nfs-utils \
-  git docker-buildx screen avahi samba virt-manager \
-  cockpit-system cockpit-ostree cockpit-podman cockpit-storaged cockpit-networkmanager cockpit-machines cockpit-selinux cockpit-files
+rpm-ostree install \
+  screen avahi samba \
+  cockpit-system cockpit-ostree cockpit-podman cockpit-networkmanager cockpit-selinux cockpit-files
 podman container runlabel --name cockpit-ws RUN quay.io/cockpit/ws
 podman container runlabel INSTALL quay.io/cockpit/ws
 
 echo Enable units after rebooting
 echo $ sudo passwd googol
-echo $ systemctl enable libvirtd.service
 echo $ systemctl enable cockpit.service
