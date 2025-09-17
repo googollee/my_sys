6ccbc5e0345f471238ab952855f6ad370ea6117b
Googol Lee <i@googol.im>
2025-09-17 15:14:27 +0200

    Cleanup cockpit.

--- home-cluster/coreos.files/install_rocm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rpm-ostree install rocminfo rocm-opencl rocm-clinfo rocm-hip radeontop
-
-echo HSA_OVERRIDE_GFX_VERSION=11.0.0
---
+++ home-cluster/coreos.files/install_samba.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rpm-ostree install screen samba
+
+echo Reboot to enable same, then run ./start_smb.sh
