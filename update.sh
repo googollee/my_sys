7048c7fd58d025ee94e27a2997e5e16d509e5d34
Googol Lee <i@googol.im>
2026-08-20 12:12:38 +0200

    Update update.sh.

--- update.sh
+++ update.sh
@@ -17,3 +17,7 @@ if command -v go >/dev/null 2>&1; then
   go install github.com/nametake/golangci-lint-langserver@latest
   go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 fi
+
+if command -v nvim >/dev/null 2>&1; then
+  nvim --headless "+Lazy! sync" +qa
+fi
